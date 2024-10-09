e34c192ca30ae8fcaf5bb22b81a5ff15eb545c3c
TheOwl740 <hsshaw2@gmail.com>
2024-10-09 17:58:55 -0400

    Reworked seed generation

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-//DATA
+//VARIABLES AND CONSTANTS
 //canvas reference
 const cs = new Canvas("canvas");
 //toolkit reference
@@ -16,51 +16,89 @@ et.tabEnabled = false;
 let gameState = "home"
 //setup iterables
 let d, dd, s, ss;
+//define spatial representation
+const spatialUnit = window.innerHeight / 64;
+//OBJECTS
 //setup image tree
 const images = {
-  buttons: {
-  },
-  tiles: {
-    grassyPlains: {
-      terrain: {
-        grass: [
-          tk.generateImage("Images/Tiles/GrassyPlains/Terrain/Grass/grass0.png"),
-          tk.generateImage("Images/Tiles/GrassyPlains/Terrain/Grass/grass1.png")
-        ]
-      },
-      structures: {
+    buttons: {
+    },
+    tiles: {
+        grassyPlains: {
+            terrain: {
+                grass: [
+                    tk.generateImage("Images/Tiles/GrassyPlains/Terrain/Grass/grass0.png"),
+                    tk.generateImage("Images/Tiles/GrassyPlains/Terrain/Grass/grass1.png")
+                ]
+            },
+            structures: {
 
-      },
-      misc: {
+            },
+            misc: {
 
-      }
+            }
+        }
     }
-  }
 };
-//setup seed data
-const seed = {
-  constant: "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170675713913216",
-  base: [],
-  full: "",
-  generateFSeed: () => {
-    let lastNum = false;
-    seed.base.forEach((currentNum) => {
-      if(lastNum !== false) {
-        seed.full += seed.constant.slice(Number(lastNum + currentNum), Number(lastNum + currentNum) + 10);
-        lastNum = currentNum;
-      } else {
-        lastNum = currentNum;
-      }
-    });
-  },
-  pseudoRandom(input, digits) {
-    if((input + digits) <= seed.full.length) {
-      returnValue = seed.full.slice(input, input + digits);
-    } else {
-      returnValue = seed.full.slice(input, seed.full.length);
-      returnValue += seed.full.slice(0, (input + digits) - seed.full.length);
+//CLASSES
+//seed class
+class Seed {
+    constructor(base) {
+        //raw seed
+        this.value = "";
+        let constant = "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170675713913216"
+        let lastNum = false;
+        let currentNum;
+        for(d = 0; d < base.length; d++) {
+            currentNum = base.slice(d, d + 1);
+            if (lastNum !== false) {
+                this.value += constant.slice(Number(lastNum + currentNum), Number(lastNum + currentNum) + 10);
+                lastNum = currentNum;
+            } else {
+                lastNum = currentNum;
+            }
+        }
     }
-    return Number(returnValue)
-  }
+    pseudoRandom(input, digits) {
+        let returnValue;
+        if ((input + digits) <= this.value.length) {
+            returnValue = this.value.slice(input, input + digits);
+        } else {
+            returnValue = this.value.slice(input, this.value.length);
+            returnValue += this.value.slice(0, (input + digits) - this.value.length);
+        }
+        return Number(returnValue)
+    }
+}
+//world
+//armor class
+class Armor {
+    constructor(tier, level, enchantment, curse) {
+        this.tier = tier;
+        this.level = level;
+        this.enchantment = enchantment;
+        this.curse = curse;
+        this.identified = false;
+    }
+    calculateReduction(damage, type) {
+        return tk.randomNum((1 * this.level) ** (1 + (this.tier / 5)), (6 * this.level) ** (1 + (this.tier / 5)))
+    }
+}
+//player class
+class Player {
+    constructor() {
+        //transform position in pixels
+        this.transform = world.tileQuery()
+        this.level = 1;
+        this.hp = {
+            current: 10,
+            max: () => {
+                return 15 + (this.level * 5)
+            }
+        };
+        this.armor = new Armor(1, 1, null, null);
+        this.weapon = null;
+        this.effects = [];
+    }
 }
 //FUNCTIONS
--- script.js
+++ script.js
@@ -12,14 +12,13 @@ function runGlobalCycle() {
       let seedEntry = prompt("Welcome. Please enter an 11 digit numerical seed or press enter for a random generated seed.")
       //assign random if prompt is an invalid seed or empty
       if(seedEntry.length !== 11 || Number(seedEntry) / Number(seedEntry) !== 1) {
-        seedEntry = "random";
-      }
-      //add values to seed
-      for(s = 0; s < 11; s++) {
-        seed.base.push(seedEntry === "random" ? tk.randomNum(0, 9).toString() : seedEntry.slice(s, s + 1));
+        seedEntry = "";
+        for(s = 0; s < 11; s++) {
+            seedEntry += tk.randomNum(0, 9).toString();
+        }
       }
       //generate full seed
-      seed.generateFSeed();
+      seed = new Seed(seedEntry);
       //start game
       gameState = "game";
     //ingame
